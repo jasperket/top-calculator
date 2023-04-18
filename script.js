43e11c02e7b42a531e03641f6509fafc1b4b6bfd
jasperket <jasperkeith1.vallecera@gmail.com>
2023-04-18 20:05:38 +0800

    Added functions for operations and input prompts

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(num1,num2) {
+    return num1+num2;
+}
+
+function subtract(num1,num2) {
+    return num1-num2;
+}
+
+function multiply(num1,num2) {
+    return num1*num2;
+}
+
+function divide(num1,num2) {
+    return num1/num2;
+}
+
+let num1 = prompt("Enter the first number:");
+let operator = prompt("Enter the operation (+,-,*,/):");
+let num2 = prompt("Enter the second number:");
