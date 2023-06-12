7ff7fbb5f95d7bc256decd86be59fc029693ea3f
jasperket <jasperkeith1.vallecera@gmail.com>
2023-06-12 19:30:59 +0800

    Can now display numbers and operations

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+let hasOperations = false;
+let completeNumbers = false;
+let expression = '';
 
+const display = document.querySelector('#display');
+
+const operations = document.querySelectorAll('.operations');
+operations.forEach(button => {
+  button.addEventListener('click', () => {
+    if(completeNumbers) {
+        completeNumbers = false;
+        addtoExpression(button);
+        hasOperations = true;
+    }
+  });
+});
+
+const numbers = document.querySelectorAll('.nums');
+
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        completeNumbers = true;
+        addtoExpression(button);
+    });
+});
+
+function addtoExpression(button) {
+    if(button.classList.contains("operations")) {
+        expression = expression + " " + button.textContent + " ";
+    } else {
+        expression = expression + button.textContent;
+    }
+    
+    display.textContent = expression;
+}
 
 function add(num1,num2) {
     return num1+num2;
